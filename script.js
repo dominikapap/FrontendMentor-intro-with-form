3e97ffa08c9fce16ee5c4126c2253f9040ab8e8a
dominikapap <dominikasz88@interia.pl>
2022-04-22 15:54:27 +0200

    added event listeners for dfocuing in and out of inputs, changed a way to get access to all inputs and errors from html

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-/*const firstName = document.getElementById("firstName");
-const firstNameErr = document.getElementById("firstNameErr");
-const lastName = document.getElementById("lastName");
-const lastNameErr = document.getElementById("lastNameErr");
-const email = document.getElementById("email");
-const emailErr = document.getElementById("emailErr");
-const password = document.getElementById("password");
-const passwordErr = document.getElementById("passwordErr");
-const form = document.getElementById("form");
-const claimBtn = document.getElementById("claimBtn");*/
-
-let formElements = [firstName, firstNameErr, lastName, lastNameErr, email, emailErr, password, passwordErr, form, claimBtn]
-
-
-for (let i = 0; i< formElements.length; i++){
-     formElements[i] = document.getElementById(`${formElements[i]}`);
-}
-
-const inputs = [
+/*const inputs = [
   {name: firstName,error: firstNameErr,message: "First name cannot be empty"},
   {name: lastName, error: lastNameErr, message: "Last name cannot be empty"},
   {name: email, error: emailErr, message: "Looks like this is not an email"},
   {name: password, error: passwordErr, message: "Password cannot be empty"},
-];
+];*/
 
+const form = document.querySelector("#form");
+const inp = document.querySelectorAll("input");
+const errors = document.querySelectorAll(".errorMsg");
+const messages = [
+  "First name cannot be empty",
+  "Last name cannot be empty",
+  "Looks like this is not an email",
+  "Password cannot be empty",
+];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  for (let i = 0; i < inputs.length; i++) {
-    if (!inputs[i].name.checkValidity()) {
-        inputs[i].name.classList.add("errorInp");
-        inputs[i].name.placeholder = "";
-        inputs[i].error.innerHTML = inputs[i].message;
+  for (let i = 0; i < inp.length; i++) {
+    if (!inp[i].checkValidity()) {
+      inp[i].classList.remove("focused");
+      inp[i].classList.add("errorInp");
+      inp[i].placeholder = "";
+      errors[i].innerHTML = messages[i];
     } else {
-        inputs[i].name.classList.remove("errorInp");
-        inputs[i].error.innerHTML = "";
+      inp[i].classList.remove("errorInp");
+      errors[i].innerHTML = "";
     }
   }
 });
 
-
+for (let i = 0; i < inp.length; i++) {
+  inp[i].addEventListener("focusin", () => {
+    inp[i].classList.add("focused");
+    errors[i].innerHTML = "";
+  });
+  inp[i].addEventListener("focusout", () => {
+    inp[i].classList.remove("focused");
+    errors[i].innerHTML = messages[i];
+  });
+}
